64f31809cb3fd3b4c1992be8e2f9b2de9e36cfe5
qiang.zhu <550497655@qq.com>
2019-03-19 21:19:32 +0800

    Update Activator.java

--- src/versioncreator/Activator.java
+++ src/versioncreator/Activator.java
@@ -1,9 +1,7 @@
 package versioncreator;
-
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
-
 /**
  * The activator class controls the plug-in life cycle
  */
@@ -12,13 +10,11 @@ public class Activator extends AbstractUIPlugin {
 	public static final String PLUGIN_ID = "VersionCreator"; //$NON-NLS-1$
 	// The shared instance
 	private static Activator plugin;
-	
 	/**
 	 * The constructor
 	 */
 	public Activator() {
 	}
-	
 	/*
 	 * (non-Javadoc)
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
@@ -27,7 +23,6 @@ public class Activator extends AbstractUIPlugin {
 		super.start(context);
 		plugin = this;
 	}
-	
 	/*
 	 * (non-Javadoc)
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
@@ -36,17 +31,14 @@ public class Activator extends AbstractUIPlugin {
 		plugin = null;
 		super.stop(context);
 	}
-	
 	/**
 	 * Returns the shared instance
 	 *
 	 * @return the shared instance
 	 */
 	public static Activator getDefault() {
-		System.out.println("你好");
 		return plugin;
 	}
-	
 	/**
 	 * Returns an image descriptor for the image file at the given
 	 * plug-in relative path
