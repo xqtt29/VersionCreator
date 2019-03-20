f4af9290555182bf075e220fde9b8ad2fcd80faa
DESKTOP-PTII7JV\zhuq <zhuqiang@creatorblue.com>
2019-03-20 10:15:15 +0800

    6

--- src/versioncreator/Activator.java
+++ src/versioncreator/Activator.java
@@ -61,4 +61,8 @@ public class Activator extends AbstractUIPlugin {
 	public static ImageDescriptor getImageDescriptor(String path) {
 		return imageDescriptorFromPlugin(PLUGIN_ID, path);
 	}
+	
+	public void func() {
+		
+	}
 }
