581a57db567a70b03e1b514889b8de30758bb2b0
DESKTOP-PTII7JV\zhuq <zhuqiang@creatorblue.com>
2019-03-20 09:40:43 +0800

    1

--- src/versioncreator/Activator.java
+++ src/versioncreator/Activator.java
@@ -1,21 +1,27 @@
 package versioncreator;
+
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
+
 /**
  * The activator class controls the plug-in life cycle
  */
+
 public class Activator extends AbstractUIPlugin {
 	// The plug-in ID
 	public static final String PLUGIN_ID = "VersionCreator"; //$NON-NLS-1$
 	// The shared instance
 	private static Activator plugin;
+	
 	/**
 	 * 
 	 * The constructor
 	 */
+	
 	public Activator() {
 	}
+	
 	/*
 	 * (non-Javadoc)
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
@@ -24,6 +30,7 @@ public class Activator extends AbstractUIPlugin {
 		super.start(context);
 		plugin = this;
 	}
+	
 	/*
 	 * (non-Javadoc)
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
@@ -32,15 +39,17 @@ public class Activator extends AbstractUIPlugin {
 		plugin = null;
 		super.stop(context);
 	}
+	
 	/**
 	 * Returns the shared instance
 	 *
 	 * @return the shared instance
 	 */
 	public static Activator getDefault() {
-		System.out.println("你1好");
+		System.out.println("你好");
 		return plugin;
 	}
+	
 	/**
 	 * Returns an image descriptor for the image file at the given
 	 * plug-in relative path
@@ -48,6 +57,7 @@ public class Activator extends AbstractUIPlugin {
 	 * @param path the path
 	 * @return the image descriptor
 	 */
+	
 	public static ImageDescriptor getImageDescriptor(String path) {
 		return imageDescriptorFromPlugin(PLUGIN_ID, path);
 	}
